503488753b9fc0eb69761a4ba5020ee6ffa05cc8
Földi Tamás <tfoldi@xsi.hu>
2023-10-15 12:51:40 +0200

    chore: initial commit

---
+++ ros2_qtcanbus/src/qtcanbus_sender.cpp
@@ -0,0 +1,27 @@
+#include <rclcpp/rclcpp.hpp>
+#include <QCoreApplication>
+#include <QTimer>
+#include <rclcpp/executor.hpp>
+#include <QObject>
+#include <libros2qt/qt_executor.h>
+#include "node.h"
+
+using namespace rclcpp;
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication a(argc, argv);
+    rclcpp::init(argc, argv);
+
+    auto server = std::make_shared<TestNode>();
+
+    QtExecutor executor;
+    executor.add_node(server);
+
+    executor.start();
+
+    auto res = a.exec();
+    printf("Exited QT thread\n");
+    rclcpp::shutdown();
+    return res;
+}
