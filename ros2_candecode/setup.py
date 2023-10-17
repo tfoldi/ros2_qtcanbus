c2082b480738665079ec4127a51476d4506d6637
Földi Tamás <tfoldi@xsi.hu>
2023-10-17 20:02:09 +0200

    feat: initial version of candecode (#3)
    * feat: initial version of candecode
    
    * chore: use diagnostics message as emited message
    
    * feat: initial release of the can decode node

---
+++ ros2_candecode/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ros2_candecode'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tfoldi',
+    maintainer_email='tfoldi@xsi.hu',
+    description='Decode CAN messages using DBC files',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'candecode = ros2_candecode.candecode_node:main'
+        ],
+    },
+)
