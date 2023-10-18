eb0ab7c279930dc0a61f41800c233b9368839c2b
Földi Tamás <tfoldi@xsi.hu>
2023-10-18 09:58:28 +0200

    chore: move cantools dependency to setup.py

--- ros2_candecode/setup.py
+++ ros2_candecode/setup.py
@@ -34,7 +34,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=["setuptools"],
+    install_requires=["setuptools", "cantools"],
     zip_safe=True,
     maintainer="tfoldi",
     maintainer_email="tfoldi@xsi.hu",
