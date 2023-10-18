aeb4b234659f70a78916f24b5f16b285841c8192
Földi Tamás <tfoldi@xsi.hu>
2023-10-18 09:44:14 +0200

    style: reformat + add header

--- ros2_candecode/setup.py
+++ ros2_candecode/setup.py
@@ -1,26 +1,47 @@
+# Copyright 2023, Tamas Foldi
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of
+#    conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+#    conditions and the following disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+#    endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from setuptools import setup
 
-package_name = 'ros2_candecode'
+package_name = "ros2_candecode"
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version="0.1.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='tfoldi',
-    maintainer_email='tfoldi@xsi.hu',
-    description='Decode CAN messages using DBC files',
-    license='BSD-3-Clause',
-    tests_require=['pytest'],
+    maintainer="tfoldi",
+    maintainer_email="tfoldi@xsi.hu",
+    description="Decode CAN messages using DBC files",
+    license="BSD-3-Clause",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'candecode = ros2_candecode.candecode_node:main'
-        ],
+        "console_scripts": ["candecode = ros2_candecode.candecode_node:main"],
     },
 )
--- ros2_qtcanbus/src/qtcanbus_sender_node.h
+++ ros2_qtcanbus/src/qtcanbus_sender_node.h
@@ -1,32 +1,58 @@
+/*
+ * Copyright (c) 2023, Tamas Foldi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of mosquitto nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #pragma once
 
-#include <QCoreApplication>
-#include <QTimer>
-#include <QObject>
 #include <QCanBusDevice>
+#include <QCoreApplication>
 #include <QElapsedTimer>
+#include <QObject>
+#include <QTimer>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int32.hpp"
 #include "ros2_qtcanbus_msg/msg/q_can_bus_frame.hpp"
+#include "std_msgs/msg/int32.hpp"
 
-class QtCanbusSenderNode : public QObject, public rclcpp::Node
-{
-    Q_OBJECT
+class QtCanbusSenderNode : public QObject, public rclcpp::Node {
+  Q_OBJECT
 
-public:
-    QtCanbusSenderNode(QObject* parent=nullptr);
-    virtual ~QtCanbusSenderNode() = default;
+ public:
+  QtCanbusSenderNode(QObject* parent = nullptr);
+  virtual ~QtCanbusSenderNode() = default;
 
-private slots:
-    void readFrames();
+ private slots:
+  void readFrames();
 
-private:
+ private:
+  QCanBusDevice* m_canDevice = nullptr;
 
-    QCanBusDevice *m_canDevice = nullptr;
-
-    rclcpp::Publisher<ros2_qtcanbus_msg::msg::QCanBusFrame>::SharedPtr m_publisher;
-    ros2_qtcanbus_msg::msg::QCanBusFrame m_message;
-
-
+  rclcpp::Publisher<ros2_qtcanbus_msg::msg::QCanBusFrame>::SharedPtr
+      m_publisher;
+  ros2_qtcanbus_msg::msg::QCanBusFrame m_message;
 };
